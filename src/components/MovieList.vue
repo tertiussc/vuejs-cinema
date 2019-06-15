91ae0c0d23d8f9d3c50bff736d7d2293e91ddb25
Tertius Scheepers <Tertiussc@gmail.com>
2019-06-15 11:06:57 +0200

    Single files

---
+++ src/components/MovieList.vue
@@ -0,0 +1,37 @@
+// HTML
+<template>
+    <div id="movie-list">
+        <div v-for="(movie, index) in filteredMovies" :key="index" class="movie">{{ movie.title }}</div>
+    </div>
+</template>
+// Vue Script
+ <script>
+import genres from "../util/genres";
+export default {
+    data() {
+        return {
+            movies: [
+                { title: "Pulp Fiction", genre: genres.CRIME },
+                { title: "Home Alone", genre: genres.COMEDY },
+                { title: "Austin Powers", genre: genres.COMEDY },
+                { title: "Kill Bill", genre: genres.HORROR }
+            ]
+        };
+    },
+    props: ["genre", "time"],
+    methods: {
+        moviePassesGenreFilter(movie) {
+            if (!this.genre.length) {
+                return true;
+            } else {
+                return this.genre.find(genre => movie.genre === genre);
+            }
+        }
+    },
+    computed: {
+        filteredMovies() {
+            return this.movies.filter(this.moviePassesGenreFilter);
+        }
+    }
+};
+</script>
